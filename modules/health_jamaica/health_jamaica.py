40cd02643fdfb486a2ed76939912c024d12a968b
Luis Falcon <falcon@gnu.org>
2014-02-25 16:43:43 +0000

    GNU HEALTH for Jamaica . Initial commit

---
+++ modules/health_jamaica/health_jamaica.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    GNU Health: The Free Health and Hospital Information System
+#    Copyright (C) 2008-2014  GNU SOLIDARIO <health@gnusolidario.org
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+#
+# Demo module to show the mechanism of creating a module
+#
+# The module creates a default random string of STRSIZE
+# for the Jamaican " Health Identification Number" - HIN
+# for demo purposes.
+#
+# The documentation of the module goes in the "doc" directory.
+
+import string
+import random
+from trytond.pyson import Eval, Not, Bool, PYSONEncoder, Equal
+from trytond.model import ModelView, ModelSQL, fields
+
+__all__ = ['PartyJHIN','AlternativePersonID','DomiciliaryUnit','Newborn','PartyPatient','Insurance']
+
+
+class PartyJHIN (ModelSQL, ModelView):
+    __name__ = 'party.party'
+
+    ref = fields.Char(
+        'PIC',
+        help='Patient Identification Code',
+        states={'invisible': Not(Bool(Eval('is_person')))})
+
+    @staticmethod
+    def default_ref():
+        # Add a default random string in the ref field.
+        # The STRSIZE constant provides the length of the HIN
+        # The format of the HIN is XXXNNNXXX
+        STRSIZE = 9
+        hin = ''
+        for x in range(STRSIZE): 
+            if ( x < 3 or x > 5 ):
+                hin = hin + random.choice(string.ascii_uppercase)
+            else:
+                hin = hin + random.choice(string.digits)
+        return hin
+
+
+
+class PartyPatient (ModelSQL, ModelView):
+    'Party'
+    __name__ = 'party.party'
+    
+    marital_status = fields.Selection([
+        (None, ''),
+        ('s', 'Single'),
+        ('m', 'Married'),
+        ('c', 'Concubinage'),
+        ('w', 'Widowed'),
+        ('d', 'Divorced'),
+        ('x', 'Separated'),
+        ('v', 'Visiting'),
+        ], 'Marital Status', sort=False)
+        
+class AlternativePersonID (ModelSQL, ModelView):
+   'Alternative person ID'
+   __name__ ='gnuhealth.person_alternative_identification' 
+   
+   name = fields.Many2One('party.party', 'Party', readonly=True)
+   alternative_id_type = fields.Selection(
+        [
+            ('trn','TRN'),
+            ('pathID','PATH ID'),
+            ('gojID','GOJ ID'),
+            ('medical recordsID','Medical Records ID'),
+        ], 'ID type', required=True, sort=False,)
+        
+   issuedby = fields.Selection(
+        [
+            ('kph','KPH'),
+            ('vjh','VJH'),
+            ('crh','CRH'),            
+        ], 'Issued By', required=False, sort=True,)
+
+class DomiciliaryUnit(ModelSQL, ModelView):
+    'Domiciliary Unit'
+    __name__ = 'gnuhealth.du'
+
+    address_subdivision = fields.Many2One(
+        'country.subdivision', 'Parish',
+        domain=[('country', '=', Eval('address_country'))],
+        depends=['address_country'], help="Enter Parish or State or County or Borrow")
+       
+class Newborn (ModelSQL, ModelView):
+    'Newborn Information'
+    __name__ = 'gnuhealth.newborn'
+    
+    bba = fields.Boolean('BBA', help='Check this box if born on arrival to health facility')
+    cephalic_perimeter = fields.Integer('Head Circumference',
+        help="Perimeter in centimeters (cm)")
+    length = fields.Integer('Crown-Heel Length', help="Length in centimeters (cm)")
+
+
+class Insurance(ModelSQL, ModelView):
+    'Insurance'
+    __name__ = 'gnuhealth.insurance' #database table, tryton translates this into gnuhealth_insurance
+    _rec_name = 'number'
+
+    # Insurance associated to an individual
+
+    name = fields.Many2One('party.party', 'Owner')
+    number = fields.Char('Policy#', required=True)
+
+    def get_rec_name(self, name):
+        return (self.company.name + ' : ' + self.number)
+
---
+++ modules/health_jamaica_jiss/health_jamaica_jiss.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    GNU Health: The Free Health and Hospital Information System
+#    Copyright (C) 2008-2014  Luis Falcon <falcon@gnu.org>
+#    Copyright (C) 2011-2014  GNU Solidario <health@gnusolidario.org>
+#
+#    MODULE : JAMAICA INJURY SURVEILLANCE SYSTEM
+# 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+#
+#
+# The documentation of the module goes in the "doc" directory.
+
+from trytond.pyson import Eval, Not, Bool, PYSONEncoder, Equal
+from trytond.model import ModelView, ModelSQL, fields
+
+__all__ = ['Jiss']
+
+
+class Jiss (ModelSQL, ModelView):
+    'Jamaica Injury Surveillance System Registration'
+    __name__ = 'gnuhealth.jiss'
+
+    name = fields.Many2One('gnuhealth.patient.evaluation',
+        'Evaluation', required=True, help='Related Patient Evaluation')
+
+    injury_date = fields.Date('Injury Date',
+        help="Usually the same as the Evaluation")
+    
+    registration_date = fields.Date('Registration Date')
+    
+    code = fields.Char('Code',help='Injury Code', required=True)
+
+    location = fields.Many2One(
+        'country.subdivision', 'Location',
+        domain=[('country', '=', 'Jamaica')])
+
+    latitude = fields.Numeric('Latidude', digits=(3, 14))
+    longitude = fields.Numeric('Longitude', digits=(4, 14))
+
+    urladdr = fields.Char(
+        'OSM Map', on_change_with=[
+            'latitude', 'longitude'],
+        help="Maps the Accident / Injury location on Open Street Map")
+
+    healthcenter = fields.Many2One('party.party','Institution',
+        domain=[('is_institution', '=', True)])
+
+    patient = fields.Function(
+        fields.Char('Patient'),
+        'get_patient', searcher='search_patient')
+
+    patient_sex = fields.Function(
+        fields.Char('Sex'),
+        'get_patient_sex')
+
+    patient_age = fields.Function(
+        fields.Char('Age'),
+        'get_patient_age')
+
+    complaint = fields.Function(
+        fields.Char('Chief Complaint'),
+        'get_patient_complaint')
+
+    injury_type = fields.Selection([
+        (None, ''),
+        ('accidental', 'Accidental'),
+        ('violence', 'Violence'),
+        ('attempt_suicide', 'Suicide Attempt'),
+        ('motor_vehicle', 'Motor Vehicle'),
+        ], 'Injury Type', required=True, sort=False)
+
+    mva_mode = fields.Selection([
+        (None, ''),
+        ('pedestrian', 'Pedestrian'),
+        ('bicycle', 'Bicycle'),
+        ('motorbike', 'Motorbike'),
+        ('car', 'Car'),
+        ('van', 'Van / Pickup / Jeep'),
+        ('truck', 'Truck / Heavy vehicle'),
+        ('bus', 'Bus'),
+        ('train', 'Train'),
+        ('taxi', 'Taxi'),
+        ('boat', 'Boat / Ship'),
+        ('aircraft', 'Aircraft'),
+        ('other', 'Other'),
+        ('unknown', 'Unknown'),
+        ], 'Mode', help="Motor Vehicle Accident Mode",sort=False,
+           states={'required': Equal(Eval('injury_type'), 'motor_vehicle')})
+
+    mva_position = fields.Selection([
+        (None, ''),
+        ('driver', 'Driver'),
+        ('passenger', 'Passenger'),
+        ('outside', 'Outside / on the back'),
+        ('bystander', 'Bystander'),
+        ('unspecified_vehicle', 'Unspecified vehicle'),
+        ('unknown', 'Unknown'),
+        ], 'User Position', help="Motor Vehicle Accident user position",sort=False,
+           states={'required': Equal(Eval('injury_type'), 'motor_vehicle')})
+ 
+
+    mva_counterpart = fields.Selection([
+        (None, ''),
+        ('pedestrian', 'Pedestrian'),
+        ('bicycle', 'Bicycle'),
+        ('motorbike', 'Motorbike'),
+        ('car', 'Car'),
+        ('van', 'Van / Pickup / Jeep'),
+        ('truck', 'Truck / Heavy vehicle'),
+        ('bus', 'Bus'),
+        ('train', 'Train'),
+        ('taxi', 'Taxi'),
+        ('boat', 'Boat / Ship'),
+        ('aircraft', 'Aircraft'),
+        ('other', 'Other'),
+        ('unknown', 'Unknown'),
+        ], 'Counterpart', help="Motor Vehicle Accident Counterpart",sort=False,
+            states={'required': Equal(Eval('injury_type'), 'motor_vehicle')})
+         
+
+    safety_gear = fields.Selection([
+        (None, ''),
+        ('yes', 'Yes'),
+        ('no', 'No'),
+        ('unknown', 'Unknown'),
+        ], 'Safety Gear', help="Use of Safety Gear - Helmet, safety belt...",sort=False,
+           states={'required': Equal(Eval('injury_type'), 'motor_vehicle')})
+
+
+    alcohol = fields.Selection([
+        (None, ''),
+        ('yes', 'Yes'),
+        ('no', 'No'),
+        ('suspected','Suspected'),
+        ('unknown', 'Unknown'),
+        ], 'Alcohol', required=True,
+            help="Use of Safety Gear - Helmet, safety belt...",sort=False)
+
+    drugs = fields.Selection([
+        (None, ''),
+        ('yes', 'Yes'),
+        ('no', 'No'),
+        ('suspected','Suspected'),
+        ('unknown', 'Unknown'),
+        ], 'Other Drugs', required=True,
+            help="Use of Safety Gear - Helmet, safety belt...",sort=False)
+
+    injury_details = fields.Text('Details')
+    
+    def get_patient(self, name):
+        return self.name.patient.name.name +' ' + self.name.patient.name.lastname
+
+    def get_patient_sex(self, name):
+        return self.name.patient.sex
+
+    def get_patient_age(self, name):
+        return self.name.patient.age
+
+    def get_patient_complaint(self, name):
+        return self.name.chief_complaint
+
+    def on_change_with_urladdr(self):
+        # Generates the URL to be used in OpenStreetMap
+        # The address will be mapped to the URL in the following way
+        # If the latitud and longitude of the Accident / Injury 
+        # are given, then those parameters will be used.
+
+        ret_url = ''
+        if (self.latitude and self.longitude):
+            ret_url = 'http://openstreetmap.org/?mlat=' + \
+                str(self.latitude) + '&mlon=' + str(self.longitude)
+
+        return ret_url
+
+    @classmethod
+    def search_patient(cls, name, clause):
+        res = []
+        value = clause[2]
+        res.append(('name.patient', clause[1], value))
+        return res
+
