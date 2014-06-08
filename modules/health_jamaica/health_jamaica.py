49e7ad4c8e5a611e2f1a025a1f5d37384601faf7
Sebastian Marro <smarro@thymbra.com>
2014-06-08 21:14:34 +0000

    health_jamaica : Add unidentified verification and UPC prefix for NN and NP

--- modules/health_jamaica/health_jamaica.py
+++ modules/health_jamaica/health_jamaica.py
@@ -31,6 +31,11 @@ from trytond.model import ModelView, ModelSQL, fields
 __all__ = ['PartyPatient','AlternativePersonID','DomiciliaryUnit','Newborn',
     'Insurance']
 
+_STATES = {
+    'invisible': Not(Bool(Eval('is_person'))),
+}
+_DEPENDS = ['is_person']
+
 
 class PartyPatient (ModelSQL, ModelView):
     'Party'
@@ -39,19 +44,23 @@ class PartyPatient (ModelSQL, ModelView):
     ref = fields.Char(
         'UPC',
         help='Universal Patient Code',
-        states={'invisible': Not(Bool(Eval('is_person')))})
-    alias = fields.Char('Pet Name', help="Common name that the Patient is referred to as")
-    middlename = fields.Char('Middle Name', help="Middle name of Patient")
-    mother_maiden_name = fields.Char('Mother Maiden Name', help="Mother's Maiden Name")
-    father_name = fields.Char('Father Name', help="Father's Name")
-    
+        states=_STATES, depends=_DEPENDS, readonly=True)
+    alias = fields.Char('Pet Name', states=_STATES, depends=_DEPENDS,
+        help="Common name that the Patient is referred to as")
+    middlename = fields.Char('Middle Name', states=_STATES, depends=_DEPENDS,
+        help="Middle name of Patient")
+    mother_maiden_name = fields.Char('Mother Maiden Name', states=_STATES, 
+        depends=_DEPENDS, help="Mother's Maiden Name")
+    father_name = fields.Char('Father Name', states=_STATES, depends=_DEPENDS,
+        help="Father's Name")
+
     suffix = fields.Selection([
         (None,''),
         ('jr', 'JR. - Junior'),
         ('sr', 'SR. - Senior'),
         ('II', 'II - The Second'),
         ('III', 'III - The Third'),
-        ], 'Suffix')
+        ], 'Suffix', states=_STATES, depends=_DEPENDS)
 
     marital_status = fields.Selection([
         (None, ''),
@@ -74,11 +83,44 @@ class PartyPatient (ModelSQL, ModelView):
 
     is_person = fields.Boolean(
         'Person',
-        on_change_with=['is_person','is_patient','is_healthprof'],
+        on_change_with=['is_person', 'is_patient', 'is_healthprof'],
         help = "Check if the party is a person.")
 
+    party_warning_ack = fields.Boolean('Party verified', 
+        states={
+            'invisible': Not(Bool(Eval('unidentified'))),
+            'readonly': Bool(Eval('ref'))
+            })
+
+    @classmethod
+    def __setup__(cls):
+        super(PartyPatient, cls).__setup__()
+        new_state = {'readonly': Bool(Eval('ref'))}
+        if new_state.keys()[0] not in cls.unidentified.states:
+            cls.unidentified.states.update(new_state)
+        if 'unidentified' not in cls.unidentified.on_change:
+            cls.unidentified.on_change.add('unidentified')
+        cls._error_messages.update({
+            'unidentified_party_warning':
+            '== UNIDENTIFIED VERIFICATION ==\n\n'
+            '- IS THE PARTY UNINDENTIFIED ? \n'
+            'Verify and check mark the party as verified\n',
+        })
+
     @staticmethod
-    def default_ref():
+    def default_party_warning_ack():
+        return True
+
+    def on_change_unidentified(self):
+        party_warning_ack = True
+        if self.unidentified:
+            party_warning_ack = False
+        return {
+            'party_warning_ack': party_warning_ack,
+        }
+
+    @classmethod
+    def generate_upc(cls):
         # Add a default random string in the ref field.
         # The STRSIZE constant provides the length of the HIN
         # The format of the UPC is XXXNNNXXX
@@ -90,6 +132,27 @@ class PartyPatient (ModelSQL, ModelView):
             else:
                 hin = hin + random.choice(string.digits)
         return hin
+
+    @classmethod
+    def create(cls, vlist):
+        vlist = [x.copy() for x in vlist]
+        for values in vlist:
+            values['ref'] = cls.generate_upc()
+            if 'unidentified' in values and values['unidentified']:
+                values['ref'] = 'NN-' + values.get('ref')
+            if 'is_person' in values and not values['is_person']:
+                values['ref'] = 'NP-' + values['ref']
+        return super(PartyPatient, cls).create(vlist)
+
+    @classmethod
+    def validate(cls, parties):
+        super(PartyPatient, cls).validate(parties)
+        for party in parties:
+            party.check_party_warning()
+
+    def check_party_warning(self):
+        if not self.party_warning_ack:
+            self.raise_user_error('unidentified_party_warning')
 
 
 class AlternativePersonID (ModelSQL, ModelView):
