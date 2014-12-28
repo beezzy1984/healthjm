c81b123b771ba2297d8a8b1493c1edd3ea27260a
Marc Murray <murraym@moh.gov.jm>
2014-12-28 10:01:12 -0500

    Fix appointment report to only show for local health professionals

--- modules/health_jamaica_sync_satellite/__init__.py
+++ modules/health_jamaica_sync_satellite/__init__.py
@@ -1,9 +1,11 @@
 from trytond.pool import Pool
 from .remote_party import RemoteParty
-from .health_professionals import HealthProfessional
+from .health_professionals import (HealthProfessional,
+                                   OpenAppointmentReportStart)
 
 def register():
     Pool.register(
         RemoteParty,
         HealthProfessional,
+        OpenAppointmentReportStart,
         module='health_jamaica_sync_satellite', type_='model')
--- modules/health_jamaica_sync_satellite/health_professionals.py
+++ modules/health_jamaica_sync_satellite/health_professionals.py
@@ -30,3 +30,26 @@ class HealthProfessional(ModelSQL, ModelView):
                     return [['OR',('name.internal_user','=',None),
                            ('institution','!=',ThisInstitution())]]
         return []
+
+
+# Models associated with HealthProfessional
+
+class OpenAppointmentReportStart(ModelView):
+    'Open Appointment Report'
+    __name__ = 'gnuhealth.appointment.report.open.start'
+
+    @classmethod
+    def __setup__(cls, *args, **kwargs):
+        hpdomain = ('is_local','=',True)
+        add_domain = True
+
+        if cls.healthprof.domain:
+            for clause in cls.healthprof.domain:
+                if clause[0] in ['is_local', 'institution']:
+                    add_domain = False
+                    break
+        else:
+            cls.healthprof.domain = []
+
+        if add_domain:
+            cls.healthprof.domain.append(hpdomain)
