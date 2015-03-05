9e5a1c585e2b5b80dc1b423304b506fb77a1dee7
Marc Murray <murraym@moh.gov.jm>
2015-03-05 07:31:21 -0500

    indentation changes based on pylint

--- modules/health_jamaica_primarycare/reports/mcsr.py
+++ modules/health_jamaica_primarycare/reports/mcsr.py
@@ -11,6 +11,7 @@ from trytond.modules.health_jamaica.wizards import StartEndDateModel
 from trytond.modules.health_jamaica.reports import BaseReport
 from .common import *
 
+
 class ClinicSummaryWizardModel(StartEndDateModel):
     '''Select date range for which you want the clinic summary report'''
     __name__ = 'healthjm_primarycare.report.clinic_summary.start'
@@ -21,21 +22,21 @@ class ClinicSummaryWizard(Wizard):
     '''Service Utilisation Report Wizard'''
     __name__ = 'healthjm_primarycare.report.clinic_summary.wizard'
     start = StateView(
-            'healthjm_primarycare.report.clinic_summary.start',
-            'health_jamaica_primarycare.report_clinic_summary_start',
-            [Button('Cancel', 'end', 'tryton-cancel'),
-             Button('Generate report', 'generate_report', 'tryton-ok',
-                    default=True)])
+        'healthjm_primarycare.report.clinic_summary.start',
+        'health_jamaica_primarycare.report_clinic_summary_start',
+        [Button('Cancel', 'end', 'tryton-cancel'),
+         Button('Generate report', 'generate_report', 'tryton-ok',
+                default=True)])
 
     generate_report = StateAction(
-                  'health_jamaica_primarycare.jmreport_clinic_summary')
+        'health_jamaica_primarycare.jmreport_clinic_summary')
 
     def transition_generate_report(self):
         return 'end'
 
     def do_generate_report(self, action):
-        data = {'start_date':self.start.on_or_after,
-                'end_date':self.start.on_or_after}
+        data = {'start_date': self.start.on_or_after,
+                'end_date': self.start.on_or_after}
 
         if self.start.on_or_before:
             data['end_date'] = self.start.on_or_before
@@ -68,54 +69,54 @@ class ClinicSummaryReport(BaseReport):
         start_date = utils.get_start_of_day(data['start_date'], tz)
         end_date = utils.get_start_of_next_day(data['end_date'], tz)
 
-        D = {} # output dictionary
+        D = {}  # output dictionary
 
         #--------------------------------------------------------
         # Section A: Curative Visits
-        search_criteria = ['AND',
-            ('institution','=',data['institution']),
+        search_criteria = [
+            'AND',
+            ('institution', '=', data['institution']),
             ('speciality.code', '=', 'CURATIVE'),
-            ('state','=','done'),
-            ('appointment_date','>=',start_date),
-            ('appointment_date','<',end_date)
+            ('state', '=', 'done'),
+            ('appointment_date', '>=', start_date),
+            ('appointment_date', '<', end_date)
         ]
 
-        appointments = Appointment.search_read(search_criteria,
-                                    order=(('speciality','ASC'),
-                                           ('appointment_date', 'ASC')),
-                                    fields_names=['id','appointment_date',
-                                                  'patient.name.sex',
-                                                  'speciality.name',
-                                                  'appointment_type'])
+        appointments = Appointment.search_read(
+            search_criteria,
+            order=(('speciality', 'ASC'), ('appointment_date', 'ASC')),
+            fields_names=['id', 'appointment_date',
+                          'patient.name.sex',
+                          'speciality.name',
+                          'appointment_type'])
         # setup the counts of appointments
-        apptcounter = Counter(map((lambda x: x['patient.name.sex']), 
-                              appointments))
+        apptcounter = Counter([x['patient.name.sex'] for x in appointments])
 
         D['A'] = apptcounter.copy()
-        D['A'].update(total = sum(apptcounter.values()))
+        D['A'].update(total=sum(apptcounter.values()))
 
         #--------------------------------------------------------
         # Newly Diagnosed and First Visits
 
         age_groups = [
-                ('<20', 0, 20), ('20-59', 20, 60), ('60+', 60, None),
-                ('Unknown', -1, None)
+            ('<20', 0, 20), ('20-59', 20, 60), ('60+', 60, None),
+            ('Unknown', -1, None)
         ]
         age_grouper = make_age_grouper(age_groups, data['start_date'],
                                        'patient.name.dob')
 
         disease_groups = [
-                ('Diabetes', ['E10 - E14.999z']),
-                ('Hypertension', [('I10 - I10.999z',
-                                   'I15', 'I15.8', 'I15.9')]),
-                ('Diabetes and Hypertension', ['E10 - E14.999z', 
-                                               ('I10 - I10.999z',
-                                                'I15', 'I15.8', 'I15.9')])
+            ('Diabetes', ['E10 - E14.999z']),
+            ('Hypertension', [('I10 - I10.999z',
+                               'I15', 'I15.8', 'I15.9')]),
+            ('Diabetes and Hypertension', ['E10 - E14.999z',
+                                           ('I10 - I10.999z',
+                                            'I15', 'I15.8', 'I15.9')])
         ]
 
         search_criteria = ['AND',
-                ('institution','=',data['institution']),
-                ('state', '=', 'done'),
+                           ('institution','=',data['institution']),
+                           ('state', '=', 'done'),
         ]
         ordering = [('first_visit_this_year', 'ASC')]
 
@@ -123,24 +124,24 @@ class ClinicSummaryReport(BaseReport):
 
         for disease_group_name, disease_diagnosis in disease_groups:
             search_clause = search_criteria[:]
-            search_clause.extend([mk_domain_clause(g,
-                                   'diagnostic_hypothesis.pathology.code')
-                                  for g in disease_diagnosis])
-            all_evals = Evaluation.search_read(search_clause, order=ordering, 
-                                               fields_names=['id',
-                                                    'patient.name.dob',
-                                                    'patient.name.sex',
-                                                    'first_visit_this_year',
-                                                    'diagnostic_hypothesis'])
-            newdiag = {'m':[],'f':[],'u':[]}
-            firstvisit = {'m':[],'f':[],'u':[]}
+            search_clause.extend([
+                mk_domain_clause(g,'diagnostic_hypothesis.pathology.code')
+                for g in disease_diagnosis
+            ])
+            all_evals = Evaluation.search_read(
+                search_clause, order=ordering,
+                fields_names=['id', 'patient.name.dob', 'patient.name.sex',
+                              'first_visit_this_year', 'diagnostic_hypothesis']
+            )
+            newdiag = {'m': [], 'f': [], 'u': []}
+            firstvisit = {'m': [], 'f': [], 'u': []}
             revisit = []
-            getsex = lambda x : x['patient.name.sex']
+            getsex = lambda x: x['patient.name.sex']
 
             for e in all_evals:
                 newly_diagnosed = False
                 diagnostic_search_parm = ['AND',
-                        ('id','in',e['diagnostic_hypothesis'])
+                        ('id', 'in', e['diagnostic_hypothesis'])
                         ] + [mk_domain_clause(g, 'pathology.code')
                              for g in disease_diagnosis]
                 e['diagnostic_hypothesis'] = DiagnosticHypothesis.search_read(
@@ -161,12 +162,12 @@ class ClinicSummaryReport(BaseReport):
                         revisit.append(e)
 
             group_counts.append({
-                    'name': disease_group_name,
-                    'new':dict([(x, Counter(map(age_grouper, y)))
-                            for x,y in newdiag.items()]),
-                    'first':dict([(x, Counter(map(age_grouper, y)))
-                                  for x,y in firstvisit.items()]),
-                    'revisit':Counter(map(getsex, revisit))})
+                'name': disease_group_name,
+                'new': dict([(x, Counter(map(age_grouper, y)))
+                            for x, y in newdiag.items()]),
+                'first': dict([(x, Counter(map(age_grouper, y)))
+                              for x, y in firstvisit.items()]),
+                'revisit': Counter(map(getsex, revisit))})
 
         D['B'] = group_counts[:]
 
@@ -182,19 +183,17 @@ class ClinicSummaryReport(BaseReport):
         # redefine age_grouper since the age groups have changed
 
         disease_groups = [
-                ('(E) Gastroenteritis', ['A09%']),
-                ('(F) Other G.I. Disorders', ['A00 - A08.999z']),
-                ('(G) Injuries', 'x'), # x marks the title
-                ('(G)(1) Intentional', ['X60 - X84.999z']),
-                ('(G)(2) Unintentional', ['Y60 - Y69.999z']),
-                ('(H) Musculoskeletal', ['M00 - M99.999z']),
-                ('(I) Leg Ulcers due to :', 'x'),
-                ('(I)(1) Diabetes', ['E10 - E14.999z', 'L97']),
-                ('(I)(2) Other Conditions', ['L97']),
-                ('(J) Genito-Urinary Diseases','x'),
-                
+            ('(E) Gastroenteritis', ['A09%']),
+            ('(F) Other G.I. Disorders', ['A00 - A08.999z']),
+            ('(G) Injuries', 'x'),  # x marks the title
+            ('(G)(1) Intentional', ['X60 - X84.999z']),
+            ('(G)(2) Unintentional', ['Y60 - Y69.999z']),
+            ('(H) Musculoskeletal', ['M00 - M99.999z']),
+            ('(I) Leg Ulcers due to :', 'x'),
+            ('(I)(1) Diabetes', ['E10 - E14.999z', 'L97']),
+            ('(I)(2) Other Conditions', ['L97']),
+            ('(J) Genito-Urinary Diseases', 'x'),
         ]
-
 
         D['C'] = group_counts[:]
 
@@ -266,13 +265,11 @@ class ClinicSummaryReport(BaseReport):
         # service_counts = service_counts.items()
         # service_counts.sort(key=lambda x: x[0])
         localcontext.update(start_date=start_date,
-                            end_date=end_date-timedelta(0,0.2),
+                            end_date=end_date-timedelta(0, 0.2),
                             D=D,
                             is_weekly=False, is_monthly=False,
-                            now_date=datetime.now(tz),)
-
-
+                            now_date=datetime.now(tz))
         # print('{}\n\n{}\n\n{}'.format('-'*80, repr(localcontext), '-'*80))
         return super(ClinicSummaryReport, cls).parse(
-                        report, records, data, localcontext
-                    )
+            report, records, data, localcontext
+        )
