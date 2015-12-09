ce719da5e54e4aa28087b3f9437856dac3d46264
Marc Murray <murraym@moh.gov.jm>
2015-12-09 10:30:44 -0500

    update a 1.2 instance to prepare for v2 icd_icpm - sql

---
+++ files/sql-migrations/0010-pathology_codes_icd_icpm.sql
@@ -0,0 +1,24 @@
+
+begin;
+
+-- We had R505 and R508 in the old version and this one converted
+-- R501 and R508 as those. However, we're gonna change it to be
+-- like the old version
+
+Update ir_model_data
+    set fs_id='R508', module='health_jamaica_icd_icpm'
+    where fs_id='R500' and module='health_icd10';
+
+Update ir_model_data
+    set fs_id='R505', module='health_jamaica_icd_icpm'
+    where fs_id='R505' and module='health_icd10';
+
+Update ir_model_data
+    set fs_id='R500', module='health_icd10'
+    where fs_id='R360' and module='health_jamaica_icd_icpm';
+
+Update ir_model_data
+    set fs_id='R501', module='health_icd10'
+    where fs_id='R361' and module='health_jamaica_icd_icpm';
+
+commit;
