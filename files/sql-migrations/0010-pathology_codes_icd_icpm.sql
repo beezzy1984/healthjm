94828254e280141ce6cd53ab6b5c3ba44678a4f0
Marc Murray <murraym@moh.gov.jm>
2015-12-11 00:01:14 -0500

    update sql script for existing version 1.2s to migrate to 2.0

--- files/sql-migrations/0010-pathology_codes_icd_icpm.sql
+++ files/sql-migrations/0010-pathology_codes_icd_icpm.sql
@@ -11,7 +11,7 @@ Update ir_model_data
 
 Update ir_model_data
     set fs_id='R505', module='health_jamaica_icd_icpm'
-    where fs_id='R505' and module='health_icd10';
+    where fs_id='R501' and module='health_icd10';
 
 Update ir_model_data
     set fs_id='R500', module='health_icd10'
